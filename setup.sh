2fc9b7f5a2d74005c35bf5bfcac0710b0c785d20
Sebastian Tauchert <sebastiantauchert@me.com>
2022-06-02 08:53:58 +0200

    Update install script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ "${SHELL}" != *"zsh"* ]]; then
+    if [[ "command -v zsh" == "" ]]; then
+        echo "zsh not found! You may have to install it first."
+    fi
+
     echo "Changing shell to 'zsh'"
-    chsh -s "$(which zsh)"
+    chsh -s "$(command -v zsh)"
 fi
 
 if [ -z "${ZSH}" ]; then
